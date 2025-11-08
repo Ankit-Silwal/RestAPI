828d13378c777d71fbb28b1cb402c19438b06550
GubbinLubbin <silwalankit27@gmail.com>
2025-11-08 18:32:57 +0530

    POST in rest api using postman

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require("express");
 const PORT=8000;
+
 const users=require('./MOCK_DATA.json');
 const app=express();
+const fs=require('fs');
+//Middleware =>Plugin
+app.use(express.urlencoded({extended:false}));
 app.get('/api/users',(req,res)=>{
   return res.json(users);
 })
@@ -18,19 +22,24 @@ app.get('/users',(req,res)=>{
   `
   res.send(html);
 })
-app.route('/api/usres/:id').get()((req,res)=>{
+app.route('/api/users/:id').get((req,res)=>{
   const id=Number(req.params.id);
   const user=users.find(user=>user.id===id);
   return res.json(user);
 }).patch((req,res)=>{
   return res.json({status:"Pending"})
 }).delete((req,res)=>{
-  return res.json({status:"Pending"})
+  return res.json({status:"success",id:users.length+1})
 })
 
 
 app.post('/api/users',(req,res)=>{
-  res.json({status:'Pending'});
+  const body=req.body;
+  users.push({...body,id:users.length+1});
+  fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+    console.log(body);
+    return res.json({status:"success",id:users.length});
+  })
 })
 
 app.listen(PORT,()=>{
