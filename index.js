73daf678a96d9c24805a6f6cf3678ea93f917e85
GubbinLubbin <silwalankit27@gmail.com>
2025-11-08 15:00:34 +0530

    Post API and information on get request

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 const express=require("express");
-const app=express();
 const PORT=8000;
-app.listen(PORT,()=>{console.log(`The server has begun at PORT ${PORT}`)})
+const users=require('./MOCK_DATA.json');
+const app=express();
+app.get('/api/users',(req,res)=>{
+  return res.json(users);
+})
+app.get('/users',(req,res)=>{
+  const html=
+  `
+  <ul>
+    ${users.map(user=>
+      `
+      <li>${user.first_name}</li>
+      `
+    ).join("")}
+  </ul>
+  `
+  res.send(html);
+})
+app.route('/api/usres/:id').get()((req,res)=>{
+  const id=Number(req.params.id);
+  const user=users.find(user=>user.id===id);
+  return res.json(user);
+}).patch((req,res)=>{
+  return res.json({status:"Pending"})
+}).delete((req,res)=>{
+  return res.json({status:"Pending"})
+})
+
+
+app.post('/api/users',(req,res)=>{
+  res.json({status:'Pending'});
+})
+
+app.listen(PORT,()=>{
+  console.log("The Server is running")
+})
